f3581d79a5474932e541c143189321a3fc77309d
mindiirova <renata.mindiyrova@mail.ru>
2024-03-17 18:25:37 +0300

    fixed bugs

--- ml_functional/infer.py
+++ ml_functional/infer.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import ast
 import torch
 import hydra
-import tqdm
+from tqdm import tqdm
 from omegaconf import DictConfig
 
 from ecglib.models.model_builder import create_model
@@ -11,28 +12,26 @@ from torch.utils.data import DataLoader
 from sklearn.metrics import classification_report
 
 
-@hydra.main(version_base=None, config_path="../ml_function", config_name="config")
+@hydra.main(version_base=None, config_path=".", config_name="config")
 def inference(cfg: DictConfig):
    
     state_dict = torch.load(cfg.checkpoint_path, map_location=torch.device("cpu"))
 
-    data_sources = cfg["dataset"]["data_sources"]
-    leads_num = 12
-
-    model_output_path = cfg.predict.save_model_output if cfg.predict.save_model_output else ''
-   
     df_test = pd.read_csv(cfg.test_path)
-    test_target = df_test.target.values
+    targets_train = [ [0.0] if 'AFIB' in ast.literal_eval(df_test.iloc[i]['scp_codes']) else [1.0] 
+           for i in range(df_test.shape[0])]
+    
+    df_test["target"] = targets_train
 
-    test_dataset = EcgDataset(df_test, test_target, data_type="npz")
+    test_dataset = EcgDataset(df_test, df_test.target.values)
 
-    model = create_model(model_name="resnet1d18", config=cfg)
+    model = create_model(model_name="resnet1d18", pathology='AFIB')
     model.load_state_dict(state_dict)
     model.to(cfg.device)
     model.eval()
 
     data_loader = DataLoader(test_dataset, batch_size=cfg.batch_size)
-
+ 
     raw_preds, true_labels = ([], [])
 
     for batch in tqdm(data_loader, total=len(data_loader)):
--- ml_functional/train.py
+++ ml_functional/train.py
@@ -1,11 +1,11 @@
 import torch
 import pandas as pd
 from tqdm import tqdm
+import ast
 
 from torch.utils.data import DataLoader
 from ecglib.data import EcgDataset
 from ecglib.models.model_builder import create_model
-from utils.dataframe_create import create_dataframe 
 from utils.losses import get_loss
 
 import hydra
@@ -13,29 +13,25 @@ import hydra
 from omegaconf import DictConfig
 
 
-
 class Trainer:
     def __init__(self, cfg):
         self.cfg = cfg
-        self.leads_num = 12
-        self.pathology_names = "AFIB"
         self.train_loader = None
         self.valid_loader = None
-        self.model = create_model(model_name="resnet1d18", config=cfg)
+        self.model = create_model(model_name="resnet1d18", pathology='AFIB')
         self.criterion = None
         self.optimizer = torch.optim.Adam(self.model.parameters())
         self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=self.optimizer)
         self.epochs = 10
-        self.early_stopping_patience = 10
-        self.device = "cpu"
+        self.device = cfg.device 
         self.augmentation = None
 
     def _init_dataloaders(self, cfg, train_df, valid_df):
         train_loader = self.get_dataset_loader(
-            train_df, cfg, self.augmentation, cfg.task_params.classes
+            train_df, cfg
         )
         valid_loader = self.get_dataset_loader(
-            valid_df, cfg, self.augmentation, cfg.task_params.classes
+            valid_df, cfg
         )
         return train_loader, valid_loader
 
@@ -44,17 +40,13 @@ class Trainer:
         self,
         df: pd.DataFrame,
         cfg,
-        classes_num,
-        augmentation=None
     ):
-        ecg_dataset = EcgDataset.for_train_from_config(
-            df, df.target.values, augmentation, cfg, classes_num
-        )
+        ecg_dataset = EcgDataset(df, df.target.values)
         ecg_loader = DataLoader(
             ecg_dataset,
-            batch_size=cfg.training_params.batch_size,
+            batch_size=cfg.batch_size,
             shuffle=True,
-            num_workers=cfg.training_params.num_workers,
+            num_workers=cfg.num_workers,
             drop_last=True,
         )
         return ecg_loader
@@ -63,9 +55,9 @@ class Trainer:
 
         for ep in range(self.epochs):
             train_loss, val_loss = self.train_epoch()
-
+            print(train_loss, val_loss)
             self.save_checkpoint()
-
+        print("Training completed!")
 
     def train_epoch(self):
         train_loss = self.train_fn()
@@ -123,20 +115,30 @@ class Trainer:
 
     def save_checkpoint(self):
     
-        checkpoint_dir = self.cfg.single_run_dir
+        checkpoint_dir = self.cfg.checkpoint_path
         checkpoint_path = f"{checkpoint_dir}/12_leads_resnet1d18_AFIB.pt"
 
         torch.save(self.model.state_dict(), checkpoint_path)
 
 
-@hydra.main(version_base=None, config_path="../ml_function", config_name="config")
+@hydra.main(version_base=None, config_path=".", config_name="config")
 def train_model(cfg: DictConfig):
    
     train_df = pd.read_csv(cfg.train_path)
     valid_df = pd.read_csv(cfg.valid_path)
     
+    targets_train = [ [0.0] if 'AFIB' in ast.literal_eval(train_df.iloc[i]['scp_codes']) else [1.0] 
+           for i in range(train_df.shape[0])]
+    
+    train_df["target"] = targets_train
+    
+    targets_valid = [ [0.0] if 'AFIB' in ast.literal_eval(valid_df.iloc[i]['scp_codes']) else [1.0] 
+           for i in range(valid_df.shape[0])]
+    
+    valid_df["target"] = targets_valid
+
     trainer = Trainer(cfg)
- 
+  
     trainer.train_loader, trainer.valid_loader = trainer._init_dataloaders(
             cfg, train_df, valid_df
         )
